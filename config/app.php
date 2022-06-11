1927931b433097ef4d0ec44e6face0c67359b6fa
habdemelash <103018187+habdemelash@users.noreply.github.com>
2022-06-11 13:37:33 -0300

    ajah

--- config/app.php
+++ config/app.php
@@ -184,6 +184,7 @@ return [
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
          App\Providers\CommandListenerProvider::class,
+    
 
     ],
 
